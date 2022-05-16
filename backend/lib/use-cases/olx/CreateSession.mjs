200be950e66198ff43bbbe430f360441a58cbcb7
Alex <45284870+alexfilatov2000@users.noreply.github.com>
2022-05-16 14:10:52 +0300

    fix olx authontication

--- backend/lib/use-cases/olx/CreateSession.mjs
+++ backend/lib/use-cases/olx/CreateSession.mjs
@@ -2,30 +2,34 @@ import Base from '../../use-cases/Base.mjs';
 
 export default class CreateSession extends Base {
     async validate(data = {}) {
-
-        console.log({data})
         const rules = {
             grant_type    : [ 'required', 'string' ],
             client_id     : [ 'required', 'string' ],
             client_secret : [ 'required', 'string' ],
             scope         : [ 'string', { default: 'v2 read write' } ],
             code          : [ 'required', 'string' ],
-            redirect_uri  : [ 'required', 'string' ]
+            redirect_uri  : [ 'required', 'string' ],
         };
 
         return this.doValidation(data, rules);
     }
 
     async execute(data) {
-        console.log(111);
 
-        const response = await fetch('https://www.olx.ua/api/open/oauth/token', {
-            method  : 'POST',
-            headers : {
-                'Content-Type' : 'application/json'
-            },
-            body : JSON.stringify(data)
-        });
+        console.log(data);
+        let response;
+        try {
+            response = await fetch('https://www.olx.ua/api/open/oauth/token', {
+                method  : 'POST',
+                headers : {
+                    'Content-Type' : 'application/json'
+                },
+                body : JSON.stringify(data)
+            });
+        } catch (e) {
+            throw e;
+        }
+
 
         const result = await response.json();
 
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,7 +8,9 @@ import RegisterVerify from "./components/auth/registerVerify";
 import Toolbar from "./components/toolbar/toolbar";
 import NotFound from "./components/utils/notfound";
 import Home from "./components/home/home";
+import Profile from "./components/home/Profile"
 import Settings from "./components/users/settings";
+import {RouteClient} from "./components/utils/route";
 
 function App(){
     return (
@@ -23,6 +25,7 @@ function App(){
                     <Route exact path="/register" element={<Register/>}/>
                     <Route exact path="/verify-email/:token" element={<RegisterVerify/>}/>
                     <Route exact path="/settings" element={<Settings/>}/>
+                    <RouteClient exact path="/profile" element={<Profile/>}/>
                     <Route exact path="/404" element={<NotFound/>}/>
                     <Route path="*" element={<NotFound/>}/>
                 </Routes>
