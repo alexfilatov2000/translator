39dc5e6e897dba21f9589be3f6dbc69bf9c7b4ad
Alex <45284870+alexfilatov2000@users.noreply.github.com>
2022-05-21 19:24:29 +0300

    Add statisctics to adverts on front

--- frontend/src/components/home/home.js
+++ frontend/src/components/home/home.js
@@ -3,8 +3,25 @@ import {useTranslation} from 'react-i18next'
 import {useDispatch, useSelector} from "react-redux";
 import * as rr from "react-redux";
 import {createSession, getAdverts} from "../../redux/modules/marketplaces";
-import {Card, CardActions, CardContent, CardMedia, Button, Typography, Box, ButtonBase} from "@mui/material";
-import * as rd from "react-router-dom";
+import moment from 'moment';
+import LocalPhoneOutlinedIcon from '@mui/icons-material/LocalPhoneOutlined';
+import RemoveRedEyeOutlinedIcon from '@mui/icons-material/RemoveRedEyeOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import {
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Button,
+    Typography,
+    Box,
+    ButtonBase,
+    FormControl,
+    InputLabel,
+    Select,
+    OutlinedInput,
+    MenuItem
+} from "@mui/material";
 import {useNavigate} from "react-router-dom";
 
 const Tr = useTranslation;
@@ -14,7 +31,7 @@ function Home() {
     const marketplaces = useSelector(state => state.marketplaces);
     const dispatch = useDispatch();
     const navigate = useNavigate();
-
+    const [filter, setFilter] = React.useState(['OLX', 'AutoRIA']);
     const olx_access_token = !!marketplaces?.olx_access_token
     const ria_access_token = !!marketplaces?.ria_access_token
     const token = !!users?.token;
@@ -49,14 +66,38 @@ function Home() {
         }
     },[dispatch]);
 
+    const handleChangeFilter = (event) => {
+        setFilter(event.target.value);
+    };
+
     if (token) {
         return (
             <Box sx={{marginLeft: 30 }}>
-                <Typography variant="h4" component="h2">
-                    {t("HOME")}
-                </Typography>
+                <Box style={{textAlign: "right"}}>
+                    <FormControl style={{right: 10, top: 30, width: 150, textAlign: "center"}}>
+                        <InputLabel id="demo-multiple-name-label">Filter</InputLabel>
+                        <Select
+                            labelId="demo-multiple-name-label"
+                            id="demo-multiple-name"
+                            multiple
+                            value={filter}
+                            onChange={handleChangeFilter}
+                            input={<OutlinedInput label="Filter" />}
+                        >
+                            { ['OLX', 'AutoRIA'].map((name) => (
+                                <MenuItem
+                                    key={name}
+                                    value={name}
+                                >
+                                    {name}
+                                </MenuItem>
+                            ))}
+                        </Select>
+                    </FormControl>
+                </Box>
 
-                {marketplaces?.adverts?.map((advert) =>
+
+                {marketplaces?.adverts?.filter(el => filter.includes(el.source)).map((advert) =>
                     <Card sx={{ width: 300, display: 'inline-block', float: 'left', margin: 2}} key={advert.id}>
                         <Box style={{position: 'relative'}}>
                             <ButtonBase style={{width:"100%"}} onClick={()=>{window.open(advert.url, "_blank")}}>
@@ -77,21 +118,29 @@ function Home() {
                                 {advert.title}
                             </Typography>
                             <Typography variant="body2" color="text.secondary" style={{textAlign:"left"}}>
-                                current prise: {advert.price}
+                                Source: {advert.source}
                             </Typography>
                             <Typography variant="body2" color="text.secondary" style={{textAlign:"left"}}>
-                                {advert.description}
+                                Prise: {advert.price}
                             </Typography>
                             <Typography variant="body2" color="text.secondary" style={{textAlign:"left"}}>
                                 status: {advert.status}
                             </Typography>
-                            <Typography variant="body2" color="text.secondary" style={{textAlign:"left"}}>
-                                date: {advert.expireDate}
+                            <Typography variant="body2" color="text.secondary" style={{textAlign:"left", color: "green"}}>
+                                Creation Date: {moment(advert.createdAt).format('L')}
                             </Typography>
+                            <Typography variant="body2" color="text.secondary" style={{textAlign:"left", color: "red"}}>
+                                Expiration Date: {moment(advert.expireDate).format('L')}
+                            </Typography>
+                            <Box style={{backgroundColor: '#e6e4e5', display: "flex", justifyContent: "center", alignContent: "center", flexDirection: "row", lineHeight: 2}}>
+                                <span style={{verticalAlign: "middle", marginRight: 20}}>Statisticts</span>
+                                <span style={{ marginRight: 5}}><RemoveRedEyeOutlinedIcon/>{advert.statistics.advert_views}</span>
+                                <span style={{ marginRight: 5}}><FavoriteBorderOutlinedIcon/>{advert.statistics.users_observing}</span>
+                                <span style={{ marginRight: 5}}><LocalPhoneOutlinedIcon/>{advert.statistics.phone_views}</span>
+                            </Box>
                         </CardContent>
-                        <CardActions>
-                            {/*<Button size="small">Share</Button>*/}
-                            <Button onClick={()=>{window.open(advert.url, "_blank")}} size="small" >Learn More</Button>
+                        <CardActions style={{float: "right"}}>
+                            <Button onClick={()=>{window.open(advert.url, "_blank")}} size="small" >GO to source</Button>
                         </CardActions>
                     </Card>
                 )}
