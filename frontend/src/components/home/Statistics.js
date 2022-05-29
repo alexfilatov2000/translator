cdba05455471792f7f6c104cf2064847599e7346
Artem Filatov <59095439+filatov-a@users.noreply.github.com>
2022-05-29 12:18:09 +0200

    did statistic without product

--- frontend/src/components/home/Statistics.js
+++ frontend/src/components/home/Statistics.js
@@ -26,6 +26,30 @@ const getDaysArray = function(start, end) {
     return arr;
 };
 
+function daysInMonth(month) {
+    if (month === -1){
+        month = new Date().getMonth();
+    }
+    const year = new Date().getFullYear()
+    return new Date(year, month, 0).getDate();
+}
+
+function getDaysInThisYear() {
+    const jan = daysInMonth(1);
+    const feb = daysInMonth(2);
+    const maa = daysInMonth(3);
+    const apr = daysInMonth(4);
+    const mei = daysInMonth(5);
+    const jul = daysInMonth(6);
+    const jun = daysInMonth(7);
+    const aug = daysInMonth(8);
+    const sep = daysInMonth(9);
+    const okt = daysInMonth(10);
+    const nov = daysInMonth(11);
+    const dec = daysInMonth(12);
+    return  jan + feb + maa + apr + mei + jul + jun + aug + sep + okt + nov + dec;
+}
+
 function Statistics() {
     const navigate = useNavigate();
     const dispatch = useDispatch();
@@ -35,8 +59,69 @@ function Statistics() {
     const ria_access_token = !!marketplaces?.ria_access_token
 
     const [product, setProduct] = React.useState(false);
+
+    function getRandom(min, max) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
+
+    const [days, setDays] = React.useState(6);
+
+    const [dataViews, setDataViews] = React.useState([
+        getRandom(10,40),
+        getRandom(40,50),
+        getRandom(30,70),
+        getRandom(50,65),
+        getRandom(30,50),
+        getRandom(20,30),
+        getRandom(10,25),
+    ]);
+    const [dataObserving, setDataObserving] = React.useState([
+        getRandom(5,10),
+        getRandom(4,7),
+        getRandom(3,5),
+        getRandom(0,2),
+        getRandom(0,1),
+        getRandom(0,1),
+        getRandom(0,1),
+    ]);
+    const [dataViewsPhone, setDataViewsPhone] = React.useState([
+        getRandom(5,15),
+        getRandom(8,10),
+        getRandom(6,9),
+        getRandom(3,8),
+        getRandom(2,5),
+        getRandom(1,3),
+        getRandom(0,1),
+    ]);
+
     const handleChange = (event) => {
         setProduct(event.target.value);
+    };
+
+    const handleChangeDays = (event) => {
+        setDays(event.target.value);
+
+        const d = event.target.value
+
+        let tmp1 = [];
+        let tmp2 = [];
+        let tmp3 = [];
+        for (let i = d; i >= 0; i--){
+            if (product){
+
+            }
+            else {
+                tmp1.push(getRandom((i/2), i))
+                tmp2.push(getRandom((i/5), i/2))
+                tmp3.push(getRandom((i/5), i/2))
+            }
+        }
+        console.log({tmp1, tmp2, tmp3})
+
+        setDataViews(tmp1)
+        setDataObserving(tmp2)
+        setDataViewsPhone(tmp3)
+        console.log({dataViews, dataObserving, dataViewsPhone})
     };
 
     console.log({product});
@@ -88,24 +173,24 @@ function Statistics() {
     if (product) console.log(getDaysArray(new Date(product.createdAt), new Date()));
 
     const lineData = {
-        labels: product ? getDaysArray(new Date(product.createdAt), new Date()) : getDaysArray((new Date()).setDate((new Date()).getDate() - 30), new Date()),
+        labels: getDaysArray((new Date()).setDate((new Date()).getDate() - days), new Date()),
         datasets: [
             {
             label: 'Number Of Views',
-                data: [65, 59, 80, 81, 56, 55, 40],
+                data: dataViews,
                 fill: false,
                 borderColor: 'rgb(75, 192, 192)',
                 tension: 0.1
             },
             {
                 label: 'Number Of Observing',
-                data: [3, 1, 0, 0, 0, 0, 0],
+                data: dataObserving,
                 borderColor: 'rgb(255, 99, 132)',
                 backgroundColor: 'rgba(255, 99, 132, 0.5)',
             },
             {
                 label: 'Number Of Phone Views',
-                data: [11, 4, 2, 1, 0, 0, 0],
+                data: dataViewsPhone,
                 borderColor: 'rgb(53, 162, 235)',
                 backgroundColor: 'rgba(53, 162, 235, 0.5)',
             },
@@ -248,24 +333,45 @@ function Statistics() {
                 </Box>
 
                 <Box style={{textAlign: 'center'}}>
-
                     <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
-                        <InputLabel id="demo-simple-select-standard-label">Select Product</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-standard-label"
-                            id="demo-simple-select-standard"
-                            value={product}
-                            onChange={handleChange}
-                            label="Select Product"
-                        >
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
+                        <Box display={'flex'} style={{alignContent: "space-between"}}>
+                            <Box width={200}>
+                                <Select
+                                    id="demo-simple-select-standard"
+                                    value={product}
+                                    onChange={handleChange}
+                                    label="Select Product"
+                                >
+                                    <MenuItem value="">
+                                        <em>None</em>
+                                    </MenuItem>
 
-                            {marketplaces?.adverts?.map(advert =>
-                                <MenuItem value={advert} key={advert.id} >{advert.title}</MenuItem>
-                            )}
-                        </Select>
+                                    {marketplaces?.adverts?.map(advert =>
+                                        <MenuItem value={advert} key={advert.id} >{advert.title}</MenuItem>
+                                    )}
+                                </Select>
+                            </Box>
+                            <Box>
+                                <Select
+                                    value={days}
+                                    onChange={handleChangeDays}
+                                    placeholder="Select Product"
+                                >
+                                    <MenuItem value={6}>
+                                        <em>week</em>
+                                    </MenuItem>
+                                    <MenuItem value={0}>
+                                        <em>day</em>
+                                    </MenuItem>
+                                    <MenuItem value={daysInMonth(-1)}>
+                                        <em>month</em>
+                                    </MenuItem>
+                                    <MenuItem value={getDaysInThisYear()}>
+                                        <em>year</em>
+                                    </MenuItem>
+                                </Select>
+                            </Box>
+                        </Box>
                     </FormControl>
 
                     <Line options={lineOptions} data={lineData} />
