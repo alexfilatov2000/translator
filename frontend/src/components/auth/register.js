5a5ec3f2e134bfc45528b481443c2cb4695086d4
filatov-a <a.filatov.xxi@gamil.com>
2022-06-19 21:37:10 +0200

    Update register.js

--- frontend/src/components/auth/register.js
+++ frontend/src/components/auth/register.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {sendRegister, sendUpdate} from "../../redux/modules/users";
+import {sendRegister} from "../../redux/modules/users";
 import * as rr from "react-redux";
 import * as rd from "react-router-dom";
 import * as r from "react";
@@ -13,21 +13,24 @@ function Register() {
     const dispatch = rr.useDispatch();
     const users = rr.useSelector(state => state.users);
 
+    const [email, setEmail] = r.useState('');
     const [password, setPassword] = r.useState('');
     const navigate = rd.useNavigate();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        const User = {password};
-        dispatch(sendUpdate({user: User}));
+        const User = {email, password};
+        dispatch(sendRegister({user: User, navigate: navigate}));
     }
 
     const onChangePassword = (e) => setPassword(e.target.value);
+    const onChangeEmail = (e) => setEmail(e.target.value);
 
     return (
         <div style={styleAuth.Div}>
-            <h2 style={styleAuth.Title}>{t('Update password')}</h2>
+            <h2 style={styleAuth.Title}>{t('register')}</h2>
             <form onSubmit={handleSubmit} style={styleAuth.Form}>
+                <CustomInput onChange={onChangeEmail} required placeholder={t('email')} type='email'/>
                 <CustomInput onChange={onChangePassword} required placeholder={t('password')} type='password'/>
                 <Button style={styleAuth.Button} type="submit" variant='contained' color='primary'>{t("Go")}</Button>
             </form>
